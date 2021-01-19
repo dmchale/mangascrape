2ea479ddccd1275755a4464080617ec34b9e386f
Dave McHale <dmchale@gmail.com>
2021-01-19 18:26:15 -0500

    initial commit

---
+++ mangascrape.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: MangaScrape
+Plugin URI: https://www.binarytemplar.com
+Description: Download manga
+Version: 0.1
+Author: Dave McHale
+Author URI: https://www.binarytemplar.com
+Text Domain: mangascrape
+Domain Path: /languages
+*/
+
